ad834ca2b69793714b04ad9f7e74b7b0c0aaa36b
Gabriel Er <gabrielerkianrong@gmail.com>
2025-12-13 21:03:02 +0800

    Fix passing empty string into QR image

--- app/components/QRDisplay.tsx
+++ app/components/QRDisplay.tsx
@@ -5,6 +5,10 @@ import useQRStore from "../stores/useQRStore";
 const QRDisplay = () => {
   const qrCode = useQRStore((state) => state.qrCode);
 
+  // Default QR Code
+  if (qrCode == "") {
+    return <p>QR Code</p>; 
+  }
   return <img src={qrCode} alt="QR Code"></img>;
 };
 
